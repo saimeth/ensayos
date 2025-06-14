b1d9da8a747db001bdcb13d9cd8ce20cbc1102d4
saimeth <diazsaimeth@gmail.com>
2025-06-14 15:05:33 -0500

    ejercicio de python

---
+++ Ejercicios de python/saimeth.py
@@ -0,0 +1,8 @@
+#Escribe un programa que pida al usuario su nombre y edad, y luego imprima un mensaje que diga cuántos años tendrá en 5 años.
+
+nombre = input(print("Por favor ingrese su nombre "))
+edad = int(input(print("por favor ingrese su edad ")))
+
+r = edad + 5
+
+print(f"Señ@r {nombre} su edad en 5 años será de {r}")
